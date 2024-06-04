4c243c0e711670cbaad7984223b88cdb13329723
Albert DeFusco <adefusco@anaconda.com>
2024-06-03 20:13:44 -0500

    cleanup

--- hatch_conda_build/plugin.py
+++ hatch_conda_build/plugin.py
@@ -1,15 +1,23 @@
 import json
-# import tomli
 import shutil
+import sys
 import typing
 import pathlib
 import collections
 import tempfile
 import subprocess
+from typing import Optional
 
 from hatchling.builders.plugin.interface import BuilderInterface
 
 from hatch_conda_build.config import CondaBuilderConfig
+
+from grayskull.strategy.py_base import merge_setup_toml_metadata
+from grayskull.strategy.py_toml import get_all_toml_info
+from souschef.recipe import Recipe
+from grayskull.config import Configuration
+from grayskull.strategy.pypi import extract_requirements, normalize_requirements_list
+from grayskull.strategy.pypi import merge_pypi_sdist_metadata
 
 
 def normalize_pypi_packages(packages: typing.List[str]):
@@ -26,21 +34,9 @@ def normalize_pypi_packages(packages: typing.List[str]):
 
 
 def construct_meta_yaml_from_pyproject(metadata, target_config):
-    py_meta = metadata.core_raw_metadata
     conda_meta = collections.defaultdict(dict)
 
-    from grayskull.strategy.py_base import merge_setup_toml_metadata
-    from grayskull.strategy.py_toml import get_all_toml_info
-    from souschef.recipe import Recipe
-    from grayskull.config import Configuration
-    from grayskull.strategy.pypi import extract_requirements, extract_optional_requirements, normalize_requirements_list
-    from grayskull.strategy.pypi import merge_pypi_sdist_metadata
-
-    # python -c 'import build.util; build.util.project_wheel_metadata('.', isolated=True)['Version"]'
     pyproject_toml = pathlib.Path(metadata._project_file)
-
-    # import build.util
-    # wheel_metadata = build.util.project_wheel_metadata(pyproject_toml.parent, isolated=False)
 
     conda_meta["package"]["name"] = metadata.name
     conda_meta["package"]["version"] = metadata.version
@@ -48,7 +44,7 @@ def construct_meta_yaml_from_pyproject(metadata, target_config):
     recipe = Recipe(name=metadata.name, version=metadata.version)
     config = Configuration(name=metadata.name, version=metadata.version, from_local_sdist=True)
 
-    # source
+    # prepare requirements from pyproject.toml
     full_metadata = get_all_toml_info(pyproject_toml)
     merged = merge_setup_toml_metadata({}, full_metadata)
     merged2 = merge_pypi_sdist_metadata({}, merged, config)
@@ -59,10 +55,7 @@ def construct_meta_yaml_from_pyproject(metadata, target_config):
         reqs[key] = normalize_requirements_list(reqs[key], config)
 
     # package
-
     conda_meta["source"]["path"] = str(pyproject_toml.parent)
-    # with pyproject_toml.open("rb") as f:
-    #     full_metadata = tomli.load(f)
 
     # build
     conda_meta["build"]["number"] = 0
@@ -72,24 +65,10 @@ def construct_meta_yaml_from_pyproject(metadata, target_config):
     ] = "{{ PYTHON }} -m pip install --no-deps --ignore-installed . -vv"
 
     # requirements
-    # if "requires-python" in py_meta:
-    #     python_spec = f"python {py_meta['requires-python']}"
-    # else:
-    #     python_spec = "python"
-
     conda_meta["requirements"]["build"] = []
 
-    # conda_meta["requirements"]["host"] = [
-    #     python_spec,
-    #     "pip",
-    # ] + normalize_pypi_packages(full_metadata["build-system"]["requires"])
-
-    # this is a local method to drop
     conda_meta["requirements"]["host"] = normalize_pypi_packages(reqs["host"])
 
-    # conda_meta["requirements"]["run"] = [
-    #     python_spec,
-    # ] + py_meta["dependencies"]
     conda_run_deps = target_config.get("run", [])
     conda_meta["requirements"]["run"] = reqs["run"] + conda_run_deps
 
@@ -113,9 +92,8 @@ def conda_build(
     build_directory: pathlib.Path,
     output_directory: pathlib.Path,
     channels: typing.List[str],
-    default_numpy_version: str,
+    default_numpy_version: Optional[str] = None,
 ):
-    print("meta.yaml: ", meta_config)
     conda_meta_filename = build_directory / "meta.yaml"
     with conda_meta_filename.open("w") as f:
         json.dump(meta_config, f)
@@ -127,14 +105,19 @@ def conda_build(
         "--output-folder",
         str(output_directory),
         "--override-channels",
-        "--numpy",
-        default_numpy_version,
     ]
+
+    if default_numpy_version is not None:
+        command.extend(
+            [
+                "--numpy",
+                default_numpy_version,
+            ]
+        )
+
     for channel in channels:
         command += ["--channel", channel]
     print("command", command)
-
-    import sys
 
     subprocess.run(command, check=True, stderr=sys.stderr, stdout=sys.stdout)
 
@@ -156,10 +139,9 @@ class CondaBuilder(BuilderInterface):
         shutil.rmtree(directory)
 
     def build_standard(self, directory: str, **build_data: typing.Dict) -> str:
-        directory = pathlib.Path(directory) / "conda"
+        conda_bld = pathlib.Path(directory) / "conda"
 
-        target_config = self.build_config.get("targets", {}).get("conda", {})
-        conda_meta = construct_meta_yaml_from_pyproject(self.metadata, target_config)
+        conda_meta = construct_meta_yaml_from_pyproject(self.metadata, self.config.target_config)
 
         with tempfile.TemporaryDirectory() as tmpdir:
             tmpdir = pathlib.Path(tmpdir)
@@ -167,16 +149,15 @@ class CondaBuilder(BuilderInterface):
             conda_build_filename = conda_build(
                 conda_meta,
                 build_directory=tmpdir,
-                output_directory=directory,
-                channels=target_config.get("channels", ["defaults"]),
-                default_numpy_version=target_config.get(
-                    "default_numpy_version", "1.22"
+                output_directory=conda_bld,
+                channels=self.target_config.get("channels", ["defaults"]),
+                default_numpy_version=self.target_config.get(
+                    "default_numpy_version", None
                 ),
             )
-            # shutil.copy2(conda_build_filename, directory / conda_build_filename.name)
-            # subprocess.run(["conda", "index", directory])
+            print(conda_build_filename)
 
-        return str(directory)
+        return "Success!"
 
     @classmethod
     def get_config_class(cls):
