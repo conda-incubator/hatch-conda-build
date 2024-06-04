c1233bb873101f592ba4a5959a52e857806c4e09
Albert DeFusco <adefusco@anaconda.com>
2024-06-03 20:19:08 -0500

    fix hatch test

--- tests/test_hatch.py
+++ tests/test_hatch.py
@@ -2,13 +2,17 @@ import sys
 import subprocess
 
 import pytest
+from pytest import MonkeyPatch
 
 
-def hatch_build_target(target: str):
-    subprocess.run([sys.executable, "-m" "build", "-t", target], capture_output=False, check=True)
+def hatch_build_target(target: str) -> None:
+    subprocess.run([sys.executable, "-m", "hatch", "build", "-t", target], capture_output=False, check=True)
 
 
 @pytest.mark.slow()
-def test_build(new_project):
-    new_project(name="project-a", version="0.1.0", dependencies=["requests"])
+def test_build(project_metadata_factory, monkeypatch: MonkeyPatch) -> None:
+    metadata = project_metadata_factory(name="project-a", version="0.1.0", dependencies=["requests"])
+
+    monkeypatch.chdir(metadata.root)
+
     hatch_build_target("conda")
